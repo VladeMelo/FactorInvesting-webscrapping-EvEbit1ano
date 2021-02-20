8b892e979549172843b478d6a65a8225ee6846bf
VladeMelo <vlademirjos@hotmail.com>
2021-02-20 15:21:57 -0300

    Changing headless

--- src/webScrapping.js
+++ src/webScrapping.js
@@ -5,7 +5,9 @@ const sleep = seconds =>
 
 const execute = async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  const page = await browser.newPage({
+    headless: false,
+  });
 
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
 
