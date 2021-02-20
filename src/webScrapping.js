b1b084949bd4a4e3ebd90d2890e420425c4d5722
VladeMelo <vlademirjos@hotmail.com>
2021-02-20 15:58:14 -0300

    Adding args to launch

--- src/webScrapping.js
+++ src/webScrapping.js
@@ -6,7 +6,10 @@ const sleep = seconds =>
 const execute = async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: ['--no-sandbox']
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
   });
   const page = await browser.newPage();
 
